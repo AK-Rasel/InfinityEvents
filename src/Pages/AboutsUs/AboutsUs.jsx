83362f6fcda272a571df36b3777c737fbc47f094
AK Rasel <raselak101010@gmail.com>
2023-10-08 20:04:15 +0600

    aboutUs page complete

--- src/Pages/AboutsUs/AboutsUs.jsx
+++ src/Pages/AboutsUs/AboutsUs.jsx
@@ -3,7 +3,46 @@
 const AboutsUs = () => {
     return (
         <div>
-            <img src="https://i.ibb.co/whz3YK0/headway-F2-KRf-Qf-Cqw-unsplash-1.jpg" alt="" />
+            <div>
+                <div className="hero min-h-[65vh]" style={{ backgroundImage: 'url(https://i.ibb.co/whz3YK0/headway-F2-KRf-Qf-Cqw-unsplash-1.jpg)' }}>
+                    <div className="hero-overlay bg-opacity-60"></div>
+                    <div className="hero-content text-center text-neutral-content">
+                        <div>
+                            <h1 className="mb-5 text-5xl font-bold">About Us</h1>
+                            <p className="mb-5">Welcome to 'InfinityEvents', your premier destination for exceptional entertainment experiences. we are passionate about creating memorable moments and bringing your events to life. As a dedicated team of event enthusiasts, we Concerts and Music Festivals, Theater and Performing Arts, Film Screenings and Premieres, Comedy Shows, Sports Events, from electrifying concerts to immersive theater performances.</p>
+
+                        </div>
+                    </div>
+                </div>
+                
+                <div>
+                    <div className="text-center mt-16  hero-content mx-auto">
+                        <div>
+                            <h1 className="mb-5 text-5xl font-bold">Who <span className="text-rose-700">We Are</span> </h1>
+                            <p className="mb-5">At InfinityEvents, we are not just event organizers; We are dream weavers, story makers and memory makers. Our mission is to bring joy, laughter and excitement into your life through a variety of entertainment events. With years of industry experience and a deep love for what we do, we are proud to offer you the best in entertainment.</p>
+                        </div>
+                    </div>
+                    <div className="flex justify-center"><div className="  h-48 border-r-2 border-rose-700 "></div></div>
+                    <div className="text-center mt-8  hero-content mx-auto">
+                        <div>
+                            <h1 className="mb-5 text-5xl font-bold">Our <span className="text-rose-700">Team</span></h1>
+                            <p className="mb-5">Our dedicated team consists of seasoned event planners, talented artists, technical experts, and customer service professionals. Together, we ensure that every detail of your entertainment experience is meticulously planned and flawlessly executed. Our passion for perfection is what sets us apart.</p>
+                        </div>
+                    </div>
+                    <div className="flex justify-center"><div className="  h-48 border-r-2 border-rose-700 "></div></div>
+                    <div className="text-center mt-8 hero-content mx-auto">
+                        <div>
+                            <h1 className="mb-5 text-5xl font-bold">Our <span className="text-rose-700">Equipment</span></h1>
+                            <p className="mb-5">To guarantee top-notch events, we invest in state-of-the-art equipment and technology. From high-quality sound systems and dazzling lighting setups to large LED screens and advanced stage setups, we spare no effort in creating an immersive atmosphere for our audiences.
+
+                                we believe that life is meant to be celebrated, and we are here to make your celebrations extraordinary. Join us in creating memories that will last a lifetime.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
         </div>
     );
 };
